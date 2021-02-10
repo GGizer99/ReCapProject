5817616074b1b76e3f3abe5fdf9d6a2d28c8318a
gizer dogan <gizerdogan@hotmail.com>
2021-02-10 15:25:36 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,89 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Console.WriteLine("------------GetAll--------------");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç : " + car.Description + " , Model : " +car.ModelYear + " , Günlük Fiyatı :  " + car.DailyPrice + " TL" );
+            }
+
+            Console.WriteLine("------------GetById--------------");
+
+            foreach (var car in carManager.GetById(6))
+            {
+                Console.WriteLine("Araç : " + car.Description + " , Model : " + car.ModelYear + " , Günlük Fiyatı :  " + car.DailyPrice + " TL");
+            }
+
+            Console.WriteLine("------------Add--------------");
+
+            Car car1 = new Car()
+            {
+                Id = 7,
+                BrandId = 5,
+                ColorId = 6,
+                ModelYear = 2021,
+                DailyPrice = 500,
+                Description = "Renault Megane 1.5 DCi Icon EDC"
+            };
+
+            carManager.Add(car1);
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç : " + car.Description + " , Model : " + car.ModelYear + " , Günlük Fiyatı :  " + car.DailyPrice + " TL");
+            }
+
+            Console.WriteLine("------------Update--------------");
+            
+
+            Car car2 = new Car() {
+            Id = 6,
+            BrandId = 4,
+            ColorId = 1,
+            ModelYear = 2020,
+            DailyPrice = 300,
+            Description = "Jeep Renegade 1.6 Multijet Limited AT"
+            };
+           
+            carManager.Update(car2);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç : " + car.Description + " , Model : " + car.ModelYear + " , Günlük Fiyatı :  " + car.DailyPrice + " TL");
+            }
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine(" 6 NOLU ARACIN DailyPrice ' ı güncellendi ");
+
+            Console.WriteLine("------------Delete--------------");
+
+            carManager.Delete(car2);
+            
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç : " + car.Description + " , Model : " + car.ModelYear + " , Günlük Fiyatı :  " + car.DailyPrice + " TL");
+            }
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine(car2.Description + " Aracı silindi");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car{Id = 1,BrandId = 1, ColorId = 1,ModelYear = 2020, DailyPrice = 700, Description = "Mercedes GLA Serisi 200 1.3 AMG"},
+                new Car{Id = 2,BrandId = 1, ColorId = 1,ModelYear = 2020, DailyPrice = 600, Description = "Mercedes A Serisi A180 1.4 Style"},
+                new Car{Id = 3,BrandId = 2, ColorId = 1,ModelYear = 2012, DailyPrice = 500, Description = "Honda Jazz 1.4 Joy"},
+                new Car{Id = 4,BrandId = 2, ColorId = 1,ModelYear = 2021, DailyPrice = 400, Description = "Honda Civic 1.5 Executive Plus CVT"},
+                new Car{Id = 5,BrandId = 3, ColorId = 1,ModelYear = 2021, DailyPrice = 300, Description = "Peugeot 2008 1.2 GT-Line AT"},
+                new Car{Id = 6,BrandId = 4, ColorId = 1,ModelYear = 2020, DailyPrice = 200, Description = "Jeep Renegade 1.6 Multijet Limited AT"},
+
+
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+           
+            Car carToDelete = _cars.SingleOrDefault(c=> c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+           return _cars.Where(c => c.Id == id).ToList(); //Şarta uyan elemanları yeni bir liste haline getirir.
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Id = car.Id;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+
+        }
+    }
+}
